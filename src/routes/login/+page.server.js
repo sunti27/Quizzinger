77b755d1a646a0b7b73dc726a2e98a8643cb394f
sunti27 <sunti27@protonmail.com>
2023-04-01 14:36:44 +0200

    change login

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,13 +1,70 @@
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms/server';
+import { z } from 'zod';
+
+const schema = z.object({
+	email: z.string(),
+	password: z.string().min(6)
+});
 
 /** @type {import('./$types').PageServerLoad} */
-export const load = async ({ url, locals: { getSession } }) => {
-	const session = await getSession();
+export const load = async (event) => {
+	const session = await event.locals.getSession();
 
     // if the user is already logged in return them to the account page
 	if (session) {
 		throw redirect(303, '/account');
 	}
 
-	return { url: url.origin };
+	const form = await superValidate(event, schema);
+
+	return { form };
+};
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	login: async (event) => {
+		const { supabase } = event.locals;
+
+		const form = await superValidate(event, schema);
+
+		if (!form.valid) {
+			return fail(400, { form })
+		}
+
+		const { email, password } = form.data;
+
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email, password
+		});
+
+		if (error) {
+			form.errors = { email: [], password: [error.message] };
+			return fail(400, { form });
+		} else {
+			throw redirect(303, '/account');
+		}
+	},
+	register: async (event) => {
+		const { supabase } = event.locals;
+
+		const form = await superValidate(event, schema);
+
+		if (!form.valid) {
+			return fail(400, { form })
+		}
+
+		const { email, password } = form.data;
+
+		const { data, error } = await supabase.auth.signUp({
+			email, password
+		});
+
+		if (error) {
+			form.errors = { email: [], password: [error.message] };
+			return fail(400, { form });
+		} else {
+			throw redirect(303, '/account');
+		}
+	}
 };
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,24 +1,54 @@
 <script>
-	import { Auth } from '@supabase/auth-ui-svelte';
+  	import { superForm } from 'sveltekit-superforms/client';
 
 	export let data;
+
+	const { form, errors, constraints, enhance, reset } = superForm(data.form, {
+		applyAction: true,
+		invalidateAll: true,
+		resetForm: false
+	});
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="row flex-center flex">
-		<div class="col-6">
-			<Auth
-				supabaseClient={data.supabase}
-				view="magic_link"
-				redirectTo={`${data.url}/loading`}
-				showLinks={false}
-				appearance={{ className: {
-					button: '!btn !variant-filled-primary !rounded !border-0',
-					input: '!input !rounded !border-0 !p-2 !variant-ghost-surface',
-					label: '!text-sm !font-semibold !label',
-					message: '!text-sm !text-center !text-token',
-				}}}
+	<form class="flex flex-col gap-4" method="POST" action="?/login" use:enhance>
+		<label class="label">
+			<span>Email</span>
+			<input
+				name="email"
+				class="input variant-form-material"
+				class:input-error={$errors.email}
+				type="email"
+				placeholder="example@mail.com"
+				data-invalid={$errors.email}
+				bind:value={$form.email}
+				{...$constraints.email}
 			/>
+			{#if $errors.email && $errors.email.length}
+				<p class="text-error">{$errors.email}</p>
+			{/if}
+		</label>
+
+		<label class="label">
+			<span>Password</span>
+			<input
+				name="password"
+				class="input variant-form-material"
+				class:input-error={$errors.password}
+				type="password"
+				placeholder="*************"
+				data-invalid={$errors.password}
+				bind:value={$form.password}
+				{...$constraints.password}
+			/>
+			{#if $errors.password && $errors.password.length}
+				<p class="text-error">{$errors.password}</p>
+			{/if}
+		</label>
+
+		<div class="flex w-full items-center gap-4">
+			<button type="submit" class="btn variant-filled-primary w-full">Login</button>
+			<button class="btn variant-ghost-primary w-full" formaction="?/register">Register</button>
 		</div>
-	</div>
+	</form>
 </div>
