aad386e32d1f529f493490233b2350d40820412a
sunti27 <sunti27@protonmail.com>
2023-04-03 12:45:57 +0200

    new dataset - server logic

---
+++ src/routes/datasets/new/+page.server.js
@@ -0,0 +1,72 @@
+import { fail, redirect } from '@sveltejs/kit';
+import { setError, superValidate } from 'sveltekit-superforms/server';
+import { z } from 'zod';
+
+const schema = z.object({
+    title: z.string(),
+    desc: z.string().optional(),
+    is_public: z.boolean(),
+    items: z.object({
+        question: z.string().min(1),
+        answer: z.string().min(1)
+    }).array().nonempty()
+})
+
+/** @type {import('./$types').PageServerLoad} */
+export const load = async (event) => {
+	const session = await event.locals.getSession();
+
+    // if the user is already logged in return them to the account page
+	if (session) {
+		throw redirect(303, '/login');
+	}
+
+	const form = await superValidate(event, schema);
+
+	return { form };
+};
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	default: async (event) => {
+		const { supabase } = event.locals;
+
+		const form = await superValidate(event, schema);
+
+		if (!form.valid) {
+			return fail(400, { form })
+		}
+
+		const { title, desc, is_public } = form.data;
+
+        const response1 = await supabase
+            .from('DataSets')
+            .insert({
+                title,
+                desc,
+                is_public
+            })
+            .select('id')
+            .single();
+
+        if (response1.error) {
+            form.errors.title = [];
+			return setError(form, 'desc', response1.error.message);
+        }
+
+        const dataset_id = response1.data.id;
+
+		const response2 = await supabase
+            .from('Entries')
+            .insert(form.data.items.map(item => ({
+                ...item,
+                dataset_id
+            })));
+
+		if (response2.error) {
+			return setError(form, 'items', response2.error.message);
+		}
+
+        return { form };
+    }
+};
--- src/routes/datasets/new/+page.svelte
+++ src/routes/datasets/new/+page.svelte
@@ -1,26 +1,64 @@
 <script>
 	import { cubicInOut } from 'svelte/easing';
 	import { crossfade } from 'svelte/transition';
+    import { SlideToggle } from '@skeletonlabs/skeleton';
+	import { superForm } from 'sveltekit-superforms/client';
 
 	const [send, receive] = crossfade({
         duration: d => Math.sqrt(d * 300),
         easing: cubicInOut,
 	});
 
-    let wrap = false;
+    export let data;
 
-    function checkWrap() {
-        wrap = question.length > 20;
-    }
+    const { form, errors, constraints } = superForm(data.form, {
+		applyAction: false,
+		invalidateAll: true,
+		resetForm: false,
+		defaultValidator: 'clear',
+	});
 
-    let question = '';
-    let answer = '';
+  import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
+
 </script>
 
-<form class="flex flex-col mx-auto my-12 max-w-[50%] justify-center">
-    <div class="relative container">
+<SuperDebug data={$form} />
+
+<form class="flex flex-col mx-auto my-12 max-w-[50%] justify-center gap-4">
+    <div class="flex items-center justify-between">
+        <label class="label">
+            <span>Title</span>
+            <input
+                class="input"
+                class:input-error={$errors.title}
+                type="text"
+                name="title"
+                placeholder="Title"
+                data-invalid={$errors.title}
+                bind:value={$form.title}
+                {...$constraints.title}
+                />
+        </label>
+        <label class="label flex flex-col" for="is_public">
+            <span>Is public?</span>
+            <SlideToggle class="data-invalid" label="description" name="is_public" bind:checked={$form.is_public} size="lg" rounded="rounded" active="bg-success-200-700-token" />
+        </label>
+    </div>
+    <div>
+        <label class="label" for="desc">Description</label>
+        <input
+            class="input"
+            class:input-error={$errors.desc}
+            type="text"
+            name="desc"
+            placeholder="Description"
+            bind:value={$form.desc}
+            {...$constraints.desc}
+            />
+    </div>
+    <!-- <div class="relative">
         {#key wrap}
-            <div class="flex justify-center gap-4 w-full absolute" id="hi" class:flex-col={wrap}>
+            <div class="flex justify-center gap-x-4 w-full absolute" id="hi" class:flex-col={wrap}>
                 <label for="question" in:receive={{key: 1}} out:send={{key: 1}}>
                     <span class="label">Question</span>
                     <input class="input" type="text" name="question" placeholder="Why ..." on:input={checkWrap} bind:value={question} />
@@ -31,5 +69,6 @@
                 </label>
             </div>
         {/key}
-    </div>
+    </div> -->
+    <button type="submit" class="btn variant-filled-primary ml-auto">Create</button>
 </form>
